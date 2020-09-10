731ee448e3bd195a5797e522056272bfb80a9359
BharatSharmapython <bharats137@gmail.com>
2020-09-10 14:46:52 +0530

    adding tic tac game project

---
+++ tictac.js
@@ -0,0 +1,154 @@
+var headone = document.querySelector('#b1');
+headone.addEventListener('click',function(){
+    headone.textContent='O';
+    headone.style.color='blue';
+    headone.style.fontSize = "500%" ;
+    headone.style.textAlign = "center";
+
+})
+
+var headtwo = document.querySelector('#b2');
+headtwo.addEventListener('click',function(){
+    headtwo.textContent='O';
+    headtwo.style.color='blue';
+        headtwo.style.fontSize = "500%" ;
+    headtwo.style.textAlign = "center";
+
+
+})
+
+var headthree = document.querySelector('#b3');
+headthree.addEventListener('click',function(){
+    headthree.textContent='O';
+    headthree.style.color='blue';
+        headthree.style.fontSize = "500%" ;
+    headthree.style.textAlign = "center";
+
+
+})
+
+var head4 = document.querySelector('#b4');
+head4.addEventListener('click',function(){
+    head4.textContent='O';
+    head4.style.color='blue';
+    head4.style.fontSize = "500%" ;
+    head4.style.textAlign = "center";
+
+})
+
+
+var head5 = document.querySelector('#b5');
+head5.addEventListener('click',function(){
+    head5.textContent='O';
+    head5.style.color='blue';
+        head5.style.fontSize = "500%" ;
+    head5.style.textAlign = "center";
+
+})
+
+
+var head6 = document.querySelector('#b6');
+head6.addEventListener('click',function(){
+    head6.textContent='O';
+    head6.style.color='blue';
+    head6.style.fontSize = "500%" ;
+    head6.style.textAlign = "center";
+
+})
+
+
+var head7 = document.querySelector('#b7');
+head7.addEventListener('click',function(){
+    head7.textContent='O';
+    head7.style.color='blue';
+    head7.style.fontSize = "500%" ;
+    head7.style.textAlign = "center";
+
+})
+
+
+var head8 = document.querySelector('#b8');
+head8.addEventListener('click',function(){
+    head8.textContent='O';
+    head8.style.color='blue';
+    head8.style.fontSize = "500%" ;
+    head8.style.textAlign = "center";
+
+})
+
+
+var head9 = document.querySelector('#b9');
+head9.addEventListener('click',function(){
+    head9.textContent='O';
+    head9.style.color='blue';
+    head9.style.fontSize = "500%" ;
+    head9.style.textAlign = "center";
+
+})
+
+
+var headx1 = document.querySelector('#b1');
+headx1.addEventListener('dblclick',function(){
+    headx1.textContent='x';
+    headx1.style.color='red';
+     headx1.style.fontSize = "500%" ;
+    headx1.style.textAlign = "center";
+})
+
+var headx2 = document.querySelector('#b2');
+headx2.addEventListener('dblclick',function(){
+    headx2.textContent='x';
+    headx2.style.color='red';
+     headx2.style.fontSize = "500%" ;
+    headx2.style.textAlign = "center";
+})
+var headx3 = document.querySelector('#b3');
+headx3.addEventListener('dblclick',function(){
+    headx3.textContent='x';
+    headx3.style.color='red';
+     headx3.style.fontSize = "500%" ;
+    headx3.style.textAlign = "center";
+})
+var headx4 = document.querySelector('#b4');
+headx4.addEventListener('dblclick',function(){
+    headx4.textContent='x';
+    headx4.style.color='red';
+     headx4.style.fontSize = "500%" ;
+    headx4.style.textAlign = "center";
+})
+var headx5 = document.querySelector('#b5');
+headx5.addEventListener('dblclick',function(){
+    headx5.textContent='x';
+    headx5.style.color='red';
+     headx5.style.fontSize = "500%" ;
+    headx5.style.textAlign = "center";
+})
+var headx6 = document.querySelector('#b6');
+headx6.addEventListener('dblclick',function(){
+    headx6.textContent='x';
+    headx6.style.color='red';
+     headx6.style.fontSize = "500%" ;
+    headx6.style.textAlign = "center";
+})
+var headx7 = document.querySelector('#b7');
+headx7.addEventListener('dblclick',function(){
+    headx7.textContent='x';
+    headx7.style.color='red';
+     headx7.style.fontSize = "500%" ;
+    headx7.style.textAlign = "center";
+})
+var headx8 = document.querySelector('#b8');
+headx8.addEventListener('dblclick',function(){
+    headx8.textContent='x';
+    headx8.style.color='red';
+     headx8.style.fontSize = "500%" ;
+    headx8.style.textAlign = "center";
+})
+var headx9 = document.querySelector('#b9');
+headx9.addEventListener('dblclick',function(){
+    headx9.textContent='x';
+    headx9.style.color='red';
+     headx9.style.fontSize = "500%" ;
+    headx9.style.textAlign = "center";
+})
+
